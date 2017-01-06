a78967290cc3bf8de368ef216c422d5574bbc2ab
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-06 17:49:48 -0600

    Title in table for edits

--- edit_surveys.php
+++ edit_surveys.php
@@ -140,41 +140,33 @@ desired effect
                   <td>1.</td>
                   <td>Update software</td>
                   <td>
-                    <div class="progress progress-xs">
-                      <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                    </div>
+                    <span class="badge bg-red">Editar</span>
                   </td>
-                  <td><span class="badge bg-red">55%</span></td>
+                  <td><span class="badge bg-red">Borrar</span></td>
                 </tr>
                 <tr>
                   <td>2.</td>
                   <td>Clean database</td>
                   <td>
-                    <div class="progress progress-xs">
-                      <div class="progress-bar progress-bar-yellow" style="width: 70%"></div>
-                    </div>
+                    <span class="badge bg-red">Editar</span>
                   </td>
-                  <td><span class="badge bg-yellow">70%</span></td>
+                  <td><span class="badge bg-red">Borrar</span></td>
                 </tr>
                 <tr>
                   <td>3.</td>
                   <td>Cron job running</td>
                   <td>
-                    <div class="progress progress-xs progress-striped active">
-                      <div class="progress-bar progress-bar-primary" style="width: 30%"></div>
-                    </div>
+                    <span class="badge bg-red">Editar</span>
                   </td>
-                  <td><span class="badge bg-light-blue">30%</span></td>
+                  <td><span class="badge bg-red">Borrar</span></td>
                 </tr>
                 <tr>
                   <td>4.</td>
                   <td>Fix and squish bugs</td>
                   <td>
-                    <div class="progress progress-xs progress-striped active">
-                      <div class="progress-bar progress-bar-success" style="width: 90%"></div>
-                    </div>
+                    <span class="badge bg-red">Editar</span>
                   </td>
-                  <td><span class="badge bg-green">90%</span></td>
+                  <td><span class="badge bg-red">Borrar</span></td>
                 </tr>
               </tbody></table>
             </div>
