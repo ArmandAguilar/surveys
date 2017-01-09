8383301aa3e9cdf1934383be736e82b006b19c85
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-09 13:03:41 -0600

    Test Save surveys 03

--- scripts/CEncuesta.php
+++ scripts/CEncuesta.php
@@ -44,7 +44,7 @@ class CEncuestas extends poolConnecion
      /* save the ask in the table AAPreguntas */
      foreach ($arrayAks as $key => $value) {
        if (!empty($vale)) {
-                $SqlAsk = "INSERT INTO [SAP].[dbo].[AAPregunta] VALUES ('$IdEcuesta','$value')"
+                $SqlAsk = "INSERT INTO [SAP].[dbo].[AAPregunta] VALUES ('$IdEcuesta','$value')";
                 $obj = new poolConnecion();
                 $con=$obj->ConexionSQLSAP();
                 $RSet=$obj->QuerySQLSAP($SqlAsk,$con);
