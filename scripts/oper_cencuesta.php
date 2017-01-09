8208adbfc791b40dd8721e22ebb6596c4741c28b
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-09 12:57:14 -0600

    Test Save surveys 01

--- scripts/oper_cencuesta.php
+++ scripts/oper_cencuesta.php
@@ -6,17 +6,18 @@ $obj = new CEncuestas();
 switch ($_GET[o])
   {
       case '1':
-              $info->$_POST[txtEncuesta];
-              $info->$_POST[txtPregunta1];
-              $info->$_POST[txtPregunta2];
-              $info->$_POST[txtPregunta3];
-              $info->$_POST[txtPregunta4];
-              $info->$_POST[txtPregunta5];
-              $info->$_POST[txtPregunta6];
-              $info->$_POST[txtPregunta7];
-              $info->$_POST[txtPregunta8];
-              $info->$_POST[txtPregunta9];
-              $info->$_POST[txtPregunta10];
+              $info->IdUsuario = $_POST[txtIdUsuario];
+              $info->Encuesta = $_POST[txtEncuesta];
+              $info->txtPregunta1 = $_POST[txtPregunta1];
+              $info->txtPregunta2 = $_POST[txtPregunta2];
+              $info->txtPregunta3 = $_POST[txtPregunta3];
+              $info->txtPregunta4 = $_POST[txtPregunta4];
+              $info->txtPregunta5 = $_POST[txtPregunta5];
+              $info->txtPregunta6 = $_POST[txtPregunta6];
+              $info->txtPregunta7 = $_POST[txtPregunta7];
+              $info->txtPregunta8 = $_POST[txtPregunta8];
+              $info->txtPregunta9 = $_POST[txtPregunta9];
+              $info->txtPregunta10 = $_POST[txtPregunta10];
               $obj->save_encuesta($info);
       break;
   }
