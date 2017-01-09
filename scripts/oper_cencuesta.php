60865ced00b2b0a9f0f6679d058ab78d94787d79
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-08 23:58:44 -0600

    11:58 pm 08/01/2017

--- scripts/oper_cencuesta.php
+++ scripts/oper_cencuesta.php
@@ -1,12 +1,12 @@
 <?php
 include("../sis.php");
 include("$path/libs/conexion.php");
-include("$path/libs/conexion.php");
-
+include("$path/scripts/CEncuesta.php");
+$obj = new CEncuestas();
 switch ($_GET[o])
   {
       case '1':
-
+              echo "Alerta";
       break;
   }
 
