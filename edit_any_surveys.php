9a2909cc36e67fbcbfe685cb1ddd672ae8cc1ace
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-13 17:57:04 -0600

    Test Edit script 55

--- edit_any_surveys.php
+++ edit_any_surveys.php
@@ -7,7 +7,7 @@ include("$path/libs/conexion.php");
 
   /* Get Id for serach the data for edit */
   $obj = new poolConnecion();
-  $Sql = "SELECT [Id],[IdEncuesta],[Pregunta] FROM [SAP].[dbo].[AAPreguntas] WHERE [IdEncuesta] = '$_GET[Id]' order by asc";
+  $Sql = "SELECT [Id],[IdEncuesta],[Pregunta] FROM [SAP].[dbo].[AAPreguntas] WHERE [IdEncuesta] = '$_GET[Id]' order by Id asc";
 
   $con=$obj->ConexionSQLSAP();
   $RSet=$obj->QuerySQLSAP($Sql,$con);
