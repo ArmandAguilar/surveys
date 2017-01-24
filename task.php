66b83a430c62fb500a1b57c055e9be74f8f5fdbe
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-24 16:02:24 -0600

    Script Task Fix it 74

--- task.php
+++ task.php
@@ -370,6 +370,6 @@ desired effect
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
      user experience. Slimscroll is required when using the
-     fixed layout. -->
+     fixed layout.    -->
 </body>
 </html>
