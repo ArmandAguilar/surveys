a786207fdc6027470dd20baa345cf1280df2387d
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-24 00:45:26 -0600

    Script Task Fix it 60

--- scripts/oper_app.php
+++ scripts/oper_app.php
@@ -4,7 +4,10 @@ include("$path/libs/conexion.php");
 switch ($_GET[o])
   {
       case '1':
-
+                  $objLastSurveysFormActualizar = new poolConnecion();
+                  $Sql="UPDATE [SAP].[dbo].[AA_Encuestado] SET [Respuesta] = '$_POST[txtValor]' WHERE [Id] = '$_POST[txtId]'";
+                  $con=$objLastSurveysFormActualizar->ConexionSQLSAP();
+                  $RSet=$objLastSurveysFormActualizar->QuerySQLSAP($Sql,$con);
           break;
   }
 ?>
