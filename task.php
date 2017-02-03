905e443286c72719e0ac0fe6ceeb91ca6d430b40
Armando Aguilar L <a.aguilar@gategeek.com>
2017-02-03 13:29:22 -0600

    03 Feb  Change Tag 12

--- task.php
+++ task.php
@@ -251,6 +251,7 @@ desired effect
                 <div class="form-group">
                    <!-- Aqui la encuesta -->
                    <div id="Pencuesta"></div>
+                  <input type="hidden" name = "txtIdEncuesta" id="txtIdEncuesta"/>
                   <input type="hidden" name = "txtIdEncuestador" id="txtIdEncuestador"  value="<?php echo $_SESSION[IdUsuario]; ?>"/>
                   <input type="hidden" name = "txtIdEncuestado" id="txtIdEncuestado" />
                   <input type="hidden" name = "txtIdTarea" id="txtIdTarea" />
