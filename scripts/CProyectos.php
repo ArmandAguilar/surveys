53f6388d1aea77a78ca1632852ff088ae9900c04
Armando Aguilar L <a.aguilar@gategeek.com>
2017-02-03 13:51:18 -0600

    03 Feb  Change Tag 19

--- scripts/CProyectos.php
+++ scripts/CProyectos.php
@@ -102,7 +102,7 @@ foreach ($claves_aleatorias as $key => $value)
       if ($value)
        {
          $j++;
-         $Sql="SELECT [Pregunta] FROM [SAP].[dbo].[AAPreguntas] Where [IdEncuesta] = '$idEncuesta' and [Id] = '$claves_aleatorias[$key]'";
+         $Sql="SELECT [Pregunta] FROM [SAP].[dbo].[AAPreguntas] Where [IdEncuesta] = '$idEncuesta' and [Id] = '$ArrayAsk[$claves_aleatorias[$key]]'";
          $objAksAll = new poolConnecion();
          $con=$objAksAll->ConexionSQLSAP();
          $RSet=$objAksAll->QuerySQLSAP($Sql,$con);
